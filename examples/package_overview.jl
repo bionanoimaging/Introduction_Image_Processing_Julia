656c4ba544bd4ec094262fac1c6209f02416534b
roflmaostc <fxw+git@mailbox.org>
2021-05-21 11:58:28 +0200

    Update notebook

--- examples/package_overview.jl
+++ examples/package_overview.jl
@@ -4,12 +4,6 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 054a0eb4-635c-403b-871b-3668778ce527
-begin
-	using Pkg
-	Pkg.add(url="https://github.com/ahnlabb/BioformatsLoader.jl")
-end
-
 # ╔═╡ 77515a38-b65b-11eb-388d-bb59872c6ca1
 using Revise, TestImages, Noise, View5D, Napari, ImageShow, Images
 
@@ -19,9 +13,6 @@ using BioformatsLoader
 # ╔═╡ 930855c0-2439-4889-a399-85b23e780b5e
 BioformatsLoader.init()
 
-# ╔═╡ 8c60880e-3cc6-43d0-837f-67ef00b96c8b
-ENV["JULIA_COPY_STACKS"] = 1
-
 # ╔═╡ d8ae01cd-7205-4155-80d4-922de74a0464
 img = testimage("resolution_test_512")
 
@@ -29,13 +20,10 @@ img = testimage("resolution_test_512")
 arr = Float32.(img)
 
 # ╔═╡ d5e27923-fd85-4354-9778-6d14f830bb46
-img_color = testimage("manduril")
-
-# ╔═╡ 7c02fc0e-a45a-4c6a-aaf6-d40a397e30c0
-
+img_color = testimage("mandurill")
 
 # ╔═╡ 46c327dd-c649-466f-b246-bca44050316d
-
+testimage("mandril_color")
 
 # ╔═╡ 7f7a302a-4c5a-4eb2-a41c-9ab0247532c6
 arr_color = permutedims(Float32.(channelview(img_color)), (2, 3, 1))
@@ -79,12 +67,9 @@ view5d(data)
 # ╠═76b435db-84e8-4d8c-9eb4-bf1bb8c18d31
 # ╠═930855c0-2439-4889-a399-85b23e780b5e
 # ╠═77515a38-b65b-11eb-388d-bb59872c6ca1
-# ╠═8c60880e-3cc6-43d0-837f-67ef00b96c8b
-# ╠═054a0eb4-635c-403b-871b-3668778ce527
 # ╠═d8ae01cd-7205-4155-80d4-922de74a0464
 # ╠═6e59a98b-a8f6-42e1-972a-85108f5f898e
 # ╠═d5e27923-fd85-4354-9778-6d14f830bb46
-# ╠═7c02fc0e-a45a-4c6a-aaf6-d40a397e30c0
 # ╠═46c327dd-c649-466f-b246-bca44050316d
 # ╠═7f7a302a-4c5a-4eb2-a41c-9ab0247532c6
 # ╠═7a6b5f57-bc33-4632-9eb0-259df20198a5
