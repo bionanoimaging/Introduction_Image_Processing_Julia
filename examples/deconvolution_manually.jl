ac0402cdbc07193af503a265f5a007af774a61ea
roflmaostc <fxw+git@mailbox.org>
2021-05-23 19:12:29 +0200

    Update first steps and deconvolution example

--- examples/deconvolution_manually.jl
+++ examples/deconvolution_manually.jl
@@ -7,6 +7,9 @@ using InteractiveUtils
 # ╔═╡ 943cd3a0-b9a7-11eb-0852-fb73c86a7ebe
 using CUDA, Zygote, Optim, Napari, TestImages, LinearAlgebra, FFTW, Noise, LineSearches, Tullio, Statistics, DeconvOptim, FourierTools
 
+# ╔═╡ 26439a7f-d22c-4b1e-a29c-5124c0a68094
+FFTW.set_num_threads(6)
+
 # ╔═╡ fd0311fb-ea86-4e07-b3f9-0038fb539cc8
 begin
 	# prevent slow scalar indexing on GPU
@@ -27,36 +30,39 @@ end
 # ╔═╡ 5a242207-d570-43c7-bfef-20dc02b5a9ea
 begin
 	img = Float32.(testimage("simple"))
-	img = abs.(resample(img, (256, 256, 256)))
+	img = abs.(resample(img, (128, 128, 128)))
 	psf = permutedims(ifftshift(Float32.(testimage("simple_3d_psf").parent)), (3, 1, 2))
-	psf = abs.(resample(psf, (256, 256, 256)))
+	psf = abs.(resample(psf, (128, 128, 128)))
 	psf ./= sum(psf)
 end;
 
 # ╔═╡ b20ca70f-a979-4a53-ba9c-bf3b91dc5261
 @view_image Float32.(testimage("simple_3d_psf").parent)
 
-# ╔═╡ 11c8ac98-14e5-4d47-acef-8e25e876fefa
-@view_image permutedims(psf, (3, 1, 2))
-
 # ╔═╡ d2dbe922-9dc1-4996-b753-62c8f7ff5a6b
-function create_forward(psf)
-	otf = rfft(psf)
-	otf_cuda = CuArray(otf)
-	function conv(a)
-		return irfft(rfft(a) .* otf, size(a, 1))
+begin
+	function create_forward(psf)
+		otf = rfft(psf)
+		
+		function conv(a)
+			return irfft(rfft(a) .* otf, size(a, 1))
+		end
+		
+		
+		otf_cuda = CuArray(otf)
+		p = plan_rfft(CuArray(psf))
+		p_inv = inv(p)
+		function conv(a::CuArray)
+			return p_inv.scale .* (p_inv.p * ((p * a) .* otf_cuda))
+		end
+		return conv
 	end
 	
-	function conv(a::CuArray)
-		return irfft(rfft(a) .* otf_cuda, size(a, 1))
-	end
-	
-	return conv
+	forward = create_forward(psf)
 end
 
 # ╔═╡ a4a8e9e9-d68e-43f2-b575-f6b30861753b
 begin
-	forward = create_forward(psf)
 	measurement = poisson(forward(img), 50)
 	measurement_cuda = CuArray(measurement)
 end;
@@ -77,15 +83,9 @@ end
 
 # ╔═╡ 043efd53-e032-4f01-ab13-37a17b9d944c
 begin
-	rec0 = mean(img) .* ones(Float32, size(img));
+	rec0 = mean(measurement) .* ones(Float32, size(measurement));
 	rec0_cuda = CuArray(rec0);
 end
-
-# ╔═╡ 8cd58df5-f5d4-4328-b15a-8e8e848a913e
-f(rec0_cuda)
-
-# ╔═╡ a4f0df6f-bdab-4ee3-af66-18843424bbe6
-reg(rec0)
 
 # ╔═╡ f198cd1d-1fb1-4e00-b135-a69b9c54872b
 begin
@@ -102,10 +102,10 @@ begin
 end;
 
 # ╔═╡ beef10c8-2c24-4be2-8396-3024ff899d09
-res = Optim.optimize(f, g!, rec0, LBFGS(linesearch=LineSearches.BackTracking()), Optim.Options(iterations=50))
+res = Optim.optimize(f, g!, rec0, LBFGS(linesearch=LineSearches.BackTracking()), Optim.Options(iterations=40))
 
 # ╔═╡ f55b0aa2-4de9-43ab-9a30-3d1a7bc85984
-res_cuda = Optim.optimize(f, g!, rec0_cuda, LBFGS(linesearch=LineSearches.BackTracking()), Optim.Options(iterations=50))
+res_cuda = Optim.optimize(f, g!, rec0_cuda, LBFGS(linesearch=LineSearches.BackTracking()), Optim.Options(iterations=40))
 
 # ╔═╡ 214fa627-9dd0-45c1-b766-77cbc2494a7d
 begin
@@ -123,21 +123,22 @@ begin
 end
 
 # ╔═╡ 08f4c9f4-d812-40e7-b32a-ef80f6e0f3a9
-@view_image ifftshift(psf)
+#@view_image ifftshift(psf)
+
+# ╔═╡ 62bf0c1a-2c67-4688-ac2f-bd932a9ae0ae
+GC.gc(true)
 
 # ╔═╡ Cell order:
 # ╠═943cd3a0-b9a7-11eb-0852-fb73c86a7ebe
+# ╠═26439a7f-d22c-4b1e-a29c-5124c0a68094
 # ╠═fd0311fb-ea86-4e07-b3f9-0038fb539cc8
 # ╠═eeb36b76-e9bc-4734-95d7-b1296f4050b1
 # ╠═5a242207-d570-43c7-bfef-20dc02b5a9ea
 # ╠═b20ca70f-a979-4a53-ba9c-bf3b91dc5261
-# ╠═11c8ac98-14e5-4d47-acef-8e25e876fefa
 # ╠═d2dbe922-9dc1-4996-b753-62c8f7ff5a6b
-# ╠═8cd58df5-f5d4-4328-b15a-8e8e848a913e
 # ╠═a4a8e9e9-d68e-43f2-b575-f6b30861753b
 # ╠═09b5ae2e-0031-44fe-a29d-2c0b0105cb65
 # ╠═7f9b85e3-a3e6-4120-8fd5-ce30debfbf82
-# ╠═a4f0df6f-bdab-4ee3-af66-18843424bbe6
 # ╠═dcc35fc0-b894-42aa-b67e-61bd66f1d7a2
 # ╠═f198cd1d-1fb1-4e00-b135-a69b9c54872b
 # ╠═043efd53-e032-4f01-ab13-37a17b9d944c
@@ -147,3 +148,4 @@ end
 # ╠═214fa627-9dd0-45c1-b766-77cbc2494a7d
 # ╠═de6df8ce-a6de-4910-a423-ec4e058bd79f
 # ╠═08f4c9f4-d812-40e7-b32a-ef80f6e0f3a9
+# ╠═62bf0c1a-2c67-4688-ac2f-bd932a9ae0ae
--- examples/first_steps.jl
+++ examples/first_steps.jl
@@ -10,7 +10,7 @@ md"## Julia Lang
 * A relatively new programming language (≈ 10 years)
 * General purpose and high level language
 * Core paradigm is *Multiple Dispatch*
-* Performance comparable to C/C++
+* Performance comparable to C/C++/Fortran
 * *for loops* don't have a performance penalty
 
 ### Reason for Speed
@@ -49,7 +49,6 @@ end
 
 # ╔═╡ 6b5f335d-15af-43f2-afc4-1b51acc693cc
 # However, we can specialize if we want
-
 function f(x::Int, y::Int)
 	return 42
 end
@@ -67,15 +66,15 @@ Consider an array where we want to sum up all positive numbers
 "
 
 # ╔═╡ aa72b577-18f6-4871-b232-d17c2bb5c7fd
-arr = randn((100_000_000));
+Γ = randn((100_000_000));
 
 # ╔═╡ 1e79336c-9f63-48ca-aa47-5c96bc7dfd9c
-result1 = sum(arr[arr .> 0.0])
+result1 = sum(Γ[Γ .> 0.0])
 
 # ╔═╡ b71710ee-09fe-4568-9aa7-fc5eaa4b0cb3
-function sum_positive(arr)
-	res = zero(eltype(arr))
-	for entry in arr
+function sum_positive(Γ)
+	res = zero(eltype(Γ))
+	for entry in Γ
 		if entry > 0
 			res += entry
 		end
@@ -84,13 +83,10 @@ function sum_positive(arr)
 end
 
 # ╔═╡ 76f78b1c-b796-4ec5-b857-12635be06a0d
-result2 = sum_positive(arr)
+result2 = sum_positive(Γ)
 
 # ╔═╡ f9f7a2fe-c0e4-412d-bc88-22b2a8cf9f4c
 result1 ≈ result2
-
-# ╔═╡ 03284aee-5c55-4124-93e5-6a4353185cfd
-md"The type of `img` is not a pure array but rather a type which encodes color information like `Gray`"
 
 # ╔═╡ Cell order:
 # ╠═efaf0218-42bd-46dd-9f93-de666df37100
@@ -105,4 +101,3 @@ md"The type of `img` is not a pure array but rather a type which encodes color i
 # ╠═76f78b1c-b796-4ec5-b857-12635be06a0d
 # ╠═b71710ee-09fe-4568-9aa7-fc5eaa4b0cb3
 # ╠═f9f7a2fe-c0e4-412d-bc88-22b2a8cf9f4c
-# ╠═03284aee-5c55-4124-93e5-6a4353185cfd
